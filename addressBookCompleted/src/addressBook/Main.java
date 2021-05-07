5cae5be942669c977e8bd3b1351ec3125e4636e1
testForMe-Now <abcodehash@gmail.com>
2021-05-07 12:32:24 +0530

    ability to create contact person with valid fields is added

--- addressBookCompleted/src/addressBook/Main.java
+++ addressBookCompleted/src/addressBook/Main.java
@@ -3,5 +3,7 @@ package addressBook;
 public class Main {
     public static void main(String args[]){
         System.out.println("welcome to addressBook");
+        AddContact toAddPersonContact = new AddContact("anurag","bhardwaj","N/a","jaipur","rajasthan","anuragcypher@gmail.com",303002,"6350391128");
+        toAddPersonContact.display();
     }
 }
