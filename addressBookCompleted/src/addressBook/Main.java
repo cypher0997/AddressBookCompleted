0ce7b3858853d2a15477c518c31994c3388748f5
testForMe-Now <abcodehash@gmail.com>
2021-05-08 11:34:33 +0530

    after this commit addressBook refactored will be added

--- addressBookCompleted/src/addressBook/Main.java
+++ addressBookCompleted/src/addressBook/Main.java
@@ -1,22 +1,8 @@
 package addressBook;
 
-import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
-        Scanner choice = new Scanner(System.in);
-        StoringMultipleContact storeMultiple_Contact = new StoringMultipleContact();
-        storeMultiple_Contact.AddContactRefactored();
-        System.out.println("enter 0 to pass this section, enter 1 to edit details, enter 2 to delete contact");
-        int input = choice.nextInt();
-        if(input == 0) {
-            System.out.println("execution ends,all details added successfully");
-        }
-        else if(input == 1) {
-           storeMultiple_Contact.editContactPersonDetail();
-        }
-        else if(input == 2){
-            storeMultiple_Contact.RemoveContactPersons();
-        }
+       System.out.println("welcome to addressBook refactored");
     }
 }
